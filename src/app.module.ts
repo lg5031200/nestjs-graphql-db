8f45e6b1f14d6f9f0e791332cfe18b01297c058c
polly <lg5031200@gmail.com>
2020-09-02 16:30:15 +0800

    add ConfigModule & env and  modify docker-compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,38 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { MongooseModule } from '@nestjs/mongoose';
 
 import { ProductModule } from './product/product.module';
-
 @Module({
   imports: [
     ProductModule,
-    MongooseModule.forRoot('mongodb://localhost:27017'),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: './schema/schema.gql',
+    }),
+    MongooseModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => {
+        const auth = `${configService.get<string>(
+          'DB_ROOT_USER',
+        )}:${configService.get<string>('DB_ROOT_PASS')}`;
+        const host = `${configService.get<string>(
+          'DB_HOST',
+        )}:${configService.get<string>('DB_PORT')}`;
+        // const db = configService.get<string>('DB_NAME');
+        const uri = `mongodb://${auth}@${host}`;
+
+        return {
+          uri,
+          // 连接字符串格式必须是mongodb：// user：password @ host：port / db
+          // 默認為 true
+          // useNewUrlParser: true,
+          // useUnifiedTopology: true,
+        };
+      },
+      inject: [ConfigService],
     }),
   ],
 })
