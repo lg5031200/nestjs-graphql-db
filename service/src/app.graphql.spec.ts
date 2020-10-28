e02eb66e48ac639fc57a57b78db5564cfba4eda1
polly <lg5031200@gmail.com>
2020-10-28 11:38:46 +0800

    add unit test

--- service/src/app.graphql.spec.ts
+++ service/src/app.graphql.spec.ts
@@ -3,7 +3,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest';
 import { AppModule } from './app.module';
 
-describe('AppController (e2e)', () => {
+describe('App Test', () => {
   let app: INestApplication;
 
   const PRODUCT = `
